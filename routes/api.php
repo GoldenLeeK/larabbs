a5b026da688df31709543e1f6da0c0cb9f002703
GoldenLeeK <614580148@qq.com>
2022-02-15 23:23:43 +0800

    接口频率限制

--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
-    //验证码
-    Route::post('verificationCodes', 'VerificationCodesController@store')
-        ->name('verificationCodes.store');
-    //用户注册
-    Route::post('users', 'UsersController@store')
-        ->name('user.store');
-});
+Route::prefix('v1')
+    ->namespace('Api')
+    ->name('api.v1.')
+    ->group(function () {
+        Route::middleware('throttle:' . config('api.rate_limits.sign'))
+            ->group(function () {
+                //验证码
+                Route::post('verificationCodes', 'VerificationCodesController@store')
+                    ->name('verificationCodes.store');
+                //用户注册
+                Route::post('users', 'UsersController@store')
+                    ->name('user.store');
+            });
+
+    });
 
 
