663accc7e051c1a67da31eaac2a2ba6ee9f201e7
GoldenLeeK <614580148@qq.com>
2022-03-13 22:33:56 +0800

    新增删除话题接口

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -26,4 +26,14 @@ class TopicsController extends Controller
         $topic->update($request->all());
         return new TopicResource($topic);
     }
+
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+
+        $topic->delete();
+
+        return response(null, 204);
+
+    }
 }
